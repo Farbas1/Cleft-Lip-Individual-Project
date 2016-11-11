6058d693f4c7b8da831c48606893aec6f8845d13
farbasmiah <farbasmiah@hotmail.co.uk>
2016-11-11 11:13:37 +0000

    App UI

---
+++ Aesthetics Tool App/platforms/browser/www/js/getcloudpatients.js
@@ -0,0 +1,23 @@
+function myFunction(arr) {
+	var i;
+	var out = '';
+	for(i = 0; i<arr.length; i++) {
+		out += 
+		'<div class="activity-item container">' + 
+			'<img src="images/faces/face1.jpg" alt="img">' + 
+			'<h5>' + arr[i].name + '</h5>' + 
+			'<em>' + arr[i].date + '</em>' + 
+			'<a href="#" class="activity-item-toggle"><i class="fa fa-plus"></i></a>' + 
+			'<div class="activity-item-detail">' + 
+				'<a href="' + arr[i].image + '"><i class="fa fa-picture-o"></i> View image</a>' + 
+				'<a href="' + arr[i].image + '" download="test"><i class="fa fa-download"></i> Download</a>' + 
+				'<a href="#"><i class="fa fa-pencil-square-o"></i> View drawings</a>' + 
+				'<a href="#"><i class="fa fa-bar-chart"></i> View symmetry scores</a>' + 
+				'<a href="#"><class="portfolio-link"><i class="fa fa-times"></i> Close</a>' + 
+				'<div class="clear"></div>' + 
+			'</div>' + 
+		'</div>' + 
+		'<div class="decoration"></div>';
+	}
+	document.getElementById("id01").innerHTML = out;
+}
---
+++ Aesthetics Tool App/www/js/cloudpatientlist.js
@@ -0,0 +1,6 @@
+myFunction([
+{"name":"Farbas Miah", "date":"7th November 2016 13:21:56", "image":"https://upload.wikimedia.org/wikipedia/commons/3/30/Googlelogo.png"},
+{"name":"Farbas Miah", "date":"6th November 2016 13:21:56", "image":"https://upload.wikimedia.org/wikipedia/commons/3/30/Googlelogo.png"},
+{"name":"Farbas Miah", "date":"5th November 2016 13:21:56", "image":"https://upload.wikimedia.org/wikipedia/commons/3/30/Googlelogo.png"},
+{"name":"Farbas Miah", "date":"4th November 2016 13:21:56", "image":"https://upload.wikimedia.org/wikipedia/commons/3/30/Googlelogo.png"},
+])
